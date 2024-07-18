6e60b9bf13ed191c589d022ae12bf3e877d3c5a2
akshay-9155 <akshayanand9162@gmail.com>
2024-07-19 00:25:24 +0530

    added coverImage in user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     },
+    coverImage: {
+        type: String,
+        default: ""
+    },
     following: {
         type: [],
         required: true
